2c956f045fe64e3c788ac5b5d70426515909ffc9
laxmiyadaav <96346006+laxmiyadaav@users.noreply.github.com>
2023-04-14 18:00:18 +0530

    Add files via upload

---
+++ base.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent('base-model', {
+    schema: {
+      radius: { type: 'number', default: 150 },
+      height: { type: 'number', default: 1 }
+    },
+    init: function () {
+  
+      this.el.setAttribute("geometry", {
+        primitive: "cylinder",
+        radius: this.data.radius,
+        height: this.data.height
+  
+      });
+      this.el.setAttribute("material", { color: "#fff" })
+  
+  
+    }
+  });
+  
---
+++ car.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('car-model', {
+    schema: {
+      model: { type: 'string', default: "../car/scene.gltf" },
+    
+    },
+    init: function () {
+  
+      this.el.setAttribute("gltf-model",this.data.model)
+      const position = {x:0,y:50,z:80}
+      const rotation = {x:0,y:-100,z:0}
+
+      this.el.setAttribute("position",position)
+      this.el.setAttribute("rotation",rotation)
+
+
+  
+  
+    }
+  });
+  
